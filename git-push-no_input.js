388280bb2374bed062bd304999d70ed9c27f0b14
Rostislav Chanoff <rostislav.chanoff@soft-fx.lv>
2024-07-19 15:11:55 +0300

    bundle script name changed

---
+++ git-push-no_input.js
@@ -0,0 +1,32 @@
+const { exec } = require('child_process');
+
+async function executeCommand(command) {
+
+  try {
+    exec(command, (error, consoleResponse, consoleError) => {
+      if (error) return console.log(`Error: ${error.message}`);
+      if (consoleError) return console.log(`Console Error: ${consoleError}`);
+
+      console.log(`Console response: ${consoleResponse}`);
+    });
+
+  } catch (error) {
+    console.log(`Failed to execute ${command}. Error: ${error}`);
+  };
+};
+
+async function runGitCommands() {
+
+  try {
+    await executeCommand('git add .');
+
+    await executeCommand(`git commit -m "bundle script name changed"`);
+
+    await executeCommand('git push origin main');
+
+  } catch (error) {
+    console.error(error);
+  };
+};
+
+runGitCommands();
